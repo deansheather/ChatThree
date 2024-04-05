7728534eeffc3935049c817046ce4a85ab9e9345
Auri <me@aurieh.me>
2024-04-05 14:09:06 +0000

    fixup! chore: cross-platform .Designer.cs generation

--- fix_resources.sh
+++ fix_resources.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# Requires the xmlstarlet and moreutils packages.
+set -euxo pipefail
+
+sed -i '\%//[[:space:]]*Runtime Version:%d' ChatThree/Resources/Language.Designer.cs
 
 for f in ChatThree/Resources/Language.resx ChatThree/Resources/Language.*.resx; do
     sed -i 's/ xml:space="preserve"//g' "$f"
